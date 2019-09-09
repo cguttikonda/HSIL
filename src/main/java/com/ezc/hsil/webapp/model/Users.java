742db6263c12c11869ce89330d2b8b2c344b4fe2
cguttikonda <cguttikonda@10.146.24.152>
2019-09-09 17:39:59 +0530

    User Change

--- src/main/java/com/ezc/hsil/webapp/model/Users.java
+++ src/main/java/com/ezc/hsil/webapp/model/Users.java
@@ -21,7 +21,7 @@ public class Users extends Auditable<String> {
 
 	
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private long id;
 	
 	
