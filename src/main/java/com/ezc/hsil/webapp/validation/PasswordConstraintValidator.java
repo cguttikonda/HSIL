95d945b052435d2b0a54bceb056879053ce3e9fd
kkakarlapudi <kakarlapudikavya@gmail.com>
2020-11-12 12:22:04 +0530

    password strength

--- src/main/java/com/ezc/hsil/webapp/validation/PasswordConstraintValidator.java
+++ src/main/java/com/ezc/hsil/webapp/validation/PasswordConstraintValidator.java
@@ -32,11 +32,11 @@ public class PasswordConstraintValidator implements ConstraintValidator<ValidPas
         final PasswordValidator validator = new PasswordValidator(Arrays.asList(
             new LengthRule(8, 30), 
             new UppercaseCharacterRule(1), 
-            new DigitCharacterRule(1), 
-            new SpecialCharacterRule(1), 
-            new NumericalSequenceRule(3,false),
-            new AlphabeticalSequenceRule(3,false),
-            new QwertySequenceRule(3,false),
+            //new DigitCharacterRule(1), 
+            //new SpecialCharacterRule(1), 
+            //new NumericalSequenceRule(3,false),
+            //new AlphabeticalSequenceRule(3,false),
+            //new QwertySequenceRule(3,false),
             new WhitespaceRule()));
         final RuleResult result = validator.validate(new PasswordData(password));
         if (result.isValid()) {
